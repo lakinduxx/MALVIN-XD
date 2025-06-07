de6d850a74a2000de7f862419e38172430515c08
lakinduxx <s.lmayamax6@gmail.com>
2025-06-07 17:37:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~F2cVCLyT#HKhBiMJag8yzBMI03bLw77Mc4pOmmfaehv3xvevp0XE",
+SESSION_ID: process.env.SESSION_ID || "malvin~5m9AVRrQ#GRrTTUaIWXEgjnwxUJeHZjH9RStzriNxPnpeWWmGfg4",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -34,7 +34,7 @@ LINK_WHITELIST: "youtube.com,github.com",
 // set the auto reply massage on status reply  
 
 
-       WELCOME: process.env.WELCOME || "true",
+       WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups 
    
        ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
